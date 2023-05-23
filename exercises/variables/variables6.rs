1322fdb1a15df01ef83a53de1977b58e38c1594b
fushouhai <fushouhai@haier.com>
2023-05-23 16:41:20 +0800

    update at 20230523_1752

--- exercises/variables/variables6.rs
+++ exercises/variables/variables6.rs
@@ -1,9 +1,10 @@
 // variables6.rs
 // Execute `rustlings hint variables6` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
-const NUMBER = 3;
+
+// missing type for `const` item
+const NUMBER: i32  = 3;
 fn main() {
     println!("Number {}", NUMBER);
 }
