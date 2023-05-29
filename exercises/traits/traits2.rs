1a608f564e84974aeb0f48a853f0ddcc65099c1e
fushouhai <fushouhai@haier.com>
2023-05-29 17:25:46 +0800

    update at 20230529_1813

--- exercises/traits/traits2.rs
+++ exercises/traits/traits2.rs
@@ -18,7 +18,12 @@ trait AppendBar {
 }
 
 // TODO: Implement trait `AppendBar` for a vector of strings.
-
+impl AppendBar for Vec<String> {
+    fn append_bar(mut self) -> Self {
+        self.push("Bar".to_owned());
+        self
+    }
+}
 #[cfg(test)]
 mod tests {
     use super::*;
